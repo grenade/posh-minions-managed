b9854e8b099b111f1d9571537a7ae94773bdf3e6
Rob Thijssen <rthijssen@gmail.com>
2019-11-28 10:59:18 +0200

    fix version string replacement

--- ci/set-version.ps1
+++ ci/set-version.ps1
@@ -1,11 +1,11 @@
 [string] $module = 'posh-minions-managed';
 [string] $path = ('{0}.psd1' -f $module);
-[string] $match = '\d+\.\d+\.\d+';
+[string] $match = "ModuleVersion = '\d+\.\d+\.\d+'";
 try {
   $version = (Find-Module -Repository 'PSGallery' -Name $module).Version.ToString().Split('.');
-  $replace = ('{0}.{1}.{2}' -f $version[0], $version[1], (([int] $version[2]) + 1));
+  $replace = ("ModuleVersion = '{0}.{1}.{2}'" -f $version[0], $version[1], (([int] $version[2]) + 1));
 } catch {
-  $replace = '0.0.1';
+  $replace = "ModuleVersion = '0.0.1'";
 }
 Write-Host ('replace "{0}" with "{1}" in "{2}"' -f $match, $replace, $path);
 $content = ((Get-Content -Path $path) | Foreach-Object { $_ -replace $match, $replace });
